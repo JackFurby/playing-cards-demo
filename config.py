95c414ece39bf5063356c718593c395e7840c3b5
Jack Furby <furbyjl@cardiff.ac.uk>
2022-09-28 13:11:53 +0100

    changed hard coded vars to enviromental vars

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
 	UPLOAD_EXTENSIONS = ['.jpg', '.jpeg', '.png']
 	UPLOAD_FOLDER = "uploads"
-	#CAMERA = "http://192.168.0.5/mjpg/video.mjpg"
-	CAMERA = "<video0>"
+	CAMERA = os.environ.get('PLAYING_CARDS_DEMO_CAMERA')
 	CONCEPT_SORT = "concept"
-	MODEL_NAME = "independent.pth"
+	MODEL_NAME = os.environ.get('PLAYING_CARDS_DEMO_MODEL')
