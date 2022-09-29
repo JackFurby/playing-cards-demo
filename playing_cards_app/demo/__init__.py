0532f6ae1d215319e98c5af8becddb5a07ad3143
Jack Furby <furbyjl@cardiff.ac.uk>
2022-09-29 14:30:45 +0100

    add upload folder cleanup on startup

--- playing_cards_app/demo/__init__.py
+++ playing_cards_app/demo/__init__.py
@@ -1,5 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, current_app
+import os
+import glob
+from config import Config  # this is probably a bad way of doing things
 
 bp = Blueprint('demo', __name__)
 
 from playing_cards_app.demo import routes
+
+# Clears the uploads folder on start-up
+uploads_folder = glob.glob(f"{os.path.join('playing_cards_app', Config.UPLOAD_FOLDER)}/*")
+for file in uploads_folder:
+	os.remove(file)
